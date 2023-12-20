74425a574b1b75a2275c04d23deb4940e93060bd
NolanDuthil <144225548+NolanDuthil@users.noreply.github.com>
2023-12-20 18:35:32 +0100

    it-5

--- main.js
+++ main.js
@@ -17,9 +17,12 @@ import { V } from "./js/view.js";
 
 // loadind data (and wait for it !)
 await M.init();
+
   
 var widgetOptions = [
     {
+        title: 'Heure de cours par semaine en MMI',
+        color: '#254552',
         debug: false,
         defaultSeries_type: 'column',
         title_label_text: 'Heures de cours par semaine en MMI',
@@ -40,8 +43,63 @@ var widgetOptions = [
             }
         ]
     },
+{
+        debug: false,
+        type: 'horizontalColumn',
+        yAxis: {
+            scale_type: 'stacked',
+            label_text: 'Heures'
+        },
+        title_label_text: 'Heures par semaine',
+        xAxis: {
+            label_text: 'Semaine',
+            categories: Object.keys(weeklyHours)
+        },
+        series: [
+            {
+                name: 'TP',
+                points: Object.values(weeklyHours).map(week => week.TP),
+                color: '#74EC8D'
+            },
+            {
+                name: 'TD',
+                points: Object.values(weeklyHours).map(week => week.TD),
+                color: '#5CAFE7'
+            },
+            {
+                name: 'CM',
+                points: Object.values(weeklyHours).map(week => week.CM),
+                color: '#F08488'
 
-
+            },
+            {
+                name: 'Autre',
+                points: Object.values(weeklyHours).map(week => week.Autre),
+                color: '#7D4FFE'
+            }
+        ]
+    },
+    {
+        debug: true,
+        defaultSeries_type: 'column',
+        title_label_text: 'Fin des cours',
+        yAxis: { label_text: 'Heures' },
+        xAxis: {
+          label_text: 'Groupe',
+          categories: ['BUT1-G1', 'BUT1-G21', 'BUT1-G22', 'BUT1-G3', 'BUT2-G1', 'BUT2-G21', 'BUT2-G22', 'BUT2-G3', 'BUT3-G1', 'BUT3-G21', 'BUT3-G22', 'BUT3-G3']
+        },
+        series: [
+          {
+            name: 'Lundi',
+            id: 's1',
+            points: [17, 16,19,19,14, 19, 19,18,19,19,19,19]
+          },
+          { name: 'Mardi', points: [19, 19,19,12,19, 19, 17,19,19,16,19,19] },
+          { name: 'Mercredi', points: [19, 19,19,17,19, 19, 19,19,19,19,11,19] },
+          { name: 'Jeudi', points: [19, 19,17,19,19, 19, 13,19,19,19,18,19] },
+          { name: 'Vendredi', points: [19, 19,19.5,19,19, 19, 17.5,19,19,19,19,19] }
+        ]
+      },
 
 
 ];
@@ -50,27 +108,8 @@ var chart1 = new JSCWidgets.BarColumn('chartDiv1', widgetOptions[0]),
 chart2 = new JSCWidgets.BarColumn('chartDiv2', widgetOptions[1]),
 chart3 = new JSCWidgets.BarColumn('chartDiv3', widgetOptions[2]),
 chart4 = new JSCWidgets.BarColumn('chartDiv4', widgetOptions[3])
-        var chart = JSC.chart('chartDiv', {
-            debug: true,
-            defaultSeries_type: 'column',
-            title_label_text: 'Fin des cours',
-            yAxis: { label_text: 'Heures' },
-            xAxis: {
-              label_text: 'Groupe',
-              categories: ['BUT1-G1', 'BUT1-G21', 'BUT1-G22', 'BUT1-G3', 'BUT2-G1', 'BUT2-G21', 'BUT2-G22', 'BUT2-G3', 'BUT3-G1', 'BUT3-G21', 'BUT3-G22', 'BUT3-G3']
-            },
-            series: [
-              {
-                name: 'Lundi',
-                id: 's1',
-                points: [17, 16,19,19,14, 19, 19,18,19,19,19,19]
-              },
-              { name: 'Mardi', points: [19, 19,19,12,19, 19, 17,19,19,16,19,19] },
-              { name: 'Mercredi', points: [19, 19,19,17,19, 19, 19,19,19,19,11,19] },
-              { name: 'Jeudi', points: [19, 19,17,19,19, 19, 13,19,19,19,18,19] },
-              { name: 'Vendredi', points: [19, 19,19.5,19,19, 19, 17.5,19,19,19,19,19] }
-            ]
-          });
+
+
           
 function handler_click(ev) {
     if (ev.target.id == 'group') {
